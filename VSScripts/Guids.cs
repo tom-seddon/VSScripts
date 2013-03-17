f8c7b1d753a80c3abb8410fe7277c26e388abaab
tom <tom@tomseddon.plus.com>
2013-03-17 14:12:24 +0000

    FIRST.

---
+++ VSScripts/Guids.cs
@@ -0,0 +1,14 @@
+﻿// Guids.cs
+// MUST match guids.h
+using System;
+
+namespace Company.VSScripts
+{
+    static class GuidList
+    {
+        public const string guidVSScriptsPkgString = "174b2dd0-5bde-4d1b-9e44-027f43e07c79";
+        public const string guidVSScriptsCmdSetString = "0ff35915-3812-4541-892b-a3ef0e316ddd";
+
+        public static readonly Guid guidVSScriptsCmdSet = new Guid(guidVSScriptsCmdSetString);
+    };
+}
---
+++ VSScripts/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Reflection;
+using System.Resources;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("VSScripts")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Company")]
+[assembly: AssemblyProduct("VSScripts")]
+[assembly: AssemblyCopyright("")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]   
+[assembly: ComVisible(false)]     
+[assembly: CLSCompliant(false)]
+[assembly: NeutralResourcesLanguage("en-US")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+
+[assembly: InternalsVisibleTo("VSScripts_IntegrationTests, PublicKey=002400000480000094000000060200000024000052534131000400000100010073a1c227b0644c9b1fb4a0dd62efb815184387cfb4cbc3b90098b38d7f15d344f64bd4be71ed6dc3d4ef2f41d07bdbd07e3d18c3d3d32a0f980c9714241f53eecabb0d0e799cb71e82b8df90614399a3b84732e0b3d7b62688742ad783e9005297d3f75ab5e6f78a0de61ffd639c4d149d44ecd81bbfca4b4cc88a4070fcf0b4")]
+[assembly: InternalsVisibleTo("VSScripts_UnitTests, PublicKey=002400000480000094000000060200000024000052534131000400000100010073a1c227b0644c9b1fb4a0dd62efb815184387cfb4cbc3b90098b38d7f15d344f64bd4be71ed6dc3d4ef2f41d07bdbd07e3d18c3d3d32a0f980c9714241f53eecabb0d0e799cb71e82b8df90614399a3b84732e0b3d7b62688742ad783e9005297d3f75ab5e6f78a0de61ffd639c4d149d44ecd81bbfca4b4cc88a4070fcf0b4")]
