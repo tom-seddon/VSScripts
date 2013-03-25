db0227d2ca53fdd69b27b099c32c61b7df25b6f6
tom <tom@tomseddon.plus.com>
2013-03-25 00:42:04 +0000

    Add ToggleTrailingBackslashes example.

---
+++ examples/ToggleTrailingBackslashes.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python
+import sys,os
+
+lines=[line.rstrip() for line in sys.stdin.readlines()]
+    
+add_bs=True
+for line in lines:
+    if line.endswith("\\"):
+        add_bs=False
+        break
+
+if add_bs:
+    tab_width=int(os.getenv("TabSize"))
+
+    widths=[]
+
+    for line in lines:
+        width=0
+        for c in line:
+            if c=='\t':
+                width=(width+tab_width)/tab_width*tab_width
+            else:
+                width+=1
+
+        widths.append(width)
+
+    max_width=max(widths)
+
+    for i in range(len(lines)-1):
+        for j in range(widths[i],
+                       max_width+1):
+            lines[i]+=" "
+
+        lines[i]+="\\"
+        
+else:
+    for i in range(len(lines)):
+        while lines[i].endswith("\\"):
+            lines[i]=lines[i][:-1]
+
+        lines[i]=lines[i].rstrip()
+    
+for line in lines:
+    print line
+    
